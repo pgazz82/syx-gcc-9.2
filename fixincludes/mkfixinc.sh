32d7cf757172ee725870106c4c2bf11de7f38f6a
Pietro Gazzillo <pgazz82@live.com>
2025-10-14 19:11:52 +0200

    Syx patches

--- fixincludes/mkfixinc.sh
+++ fixincludes/mkfixinc.sh
@@ -11,6 +11,8 @@ target=fixinc.sh
 
 # Check for special fix rules for particular targets
 case $machine in
+    *-syx* | \
+    *-*-syx* | \
     i?86-*-cygwin* | \
     i?86-*-mingw32* | \
     x86_64-*-mingw32* | \
